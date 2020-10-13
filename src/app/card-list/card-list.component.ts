1942436a3bc78e0ec695ef2877d4668acda00b13
oletendart <letendartolivia@gmail.com>
2020-10-13 21:38:26 +0200

    Exercice create button clone and remove

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { filter } from "rxjs/operators";
+
+interface Card {
+  id: number;
+  title: string;
+  about: string;
+}
 
 @Component({
-  selector: 'app-card-list',
-  templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
+  selector: "app-card-list",
+  templateUrl: "./card-list.component.html",
+  styleUrls: ["./card-list.component.css"]
 })
 export class CardListComponent implements OnInit {
+  cards: Card[] = [
+    {
+      title: "Ceci est une carte",
+      about: "Ceci est une description",
+      id: 1
+    },
+    {
+      title: "Ceci est une carte 2",
+      about: "Ceci est une description 2",
+      id: 2
+    },
+    {
+      title: "Ceci est une carte 3",
+      about: "Ceci est une description 3",
+      id: 3
+    },
+    {
+      title: "Ceci est une carte 4",
+      about: "Ceci est une description 4",
+      id: 4
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  trackCard(index: number, item: Card): string {
+    return item.id.toString();
   }
 
+  onRemove(item: Card) {
+    this.cards = this.cards.filter(c => c.id !== item.id);
+  }
+
+  onClone(item: Card) {
+    this.cards = [...this.cards, item];
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  TemplateRef
+} from "@angular/core";
 
 @Component({
   selector: "app-card",
   templateUrl: "./card.component.html",
-  styleUrls: ["./card.component.css"]
+  styleUrls: ["./card.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent implements OnInit {
   @Input() title: string;
   @Input() about: string;
+  @Input() buttonTemplate: TemplateRef<any>;
+  @Output() remove = new EventEmitter();
+
   constructor() {}
 
   ngOnInit() {}
